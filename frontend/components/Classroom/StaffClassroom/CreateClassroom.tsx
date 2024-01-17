60b90426194e4ab3b309e2ca716fedb8087ee456
AlifHossain27 <alifh044@gmail.com>
2024-01-17 18:03:49 +0600

    Adding Create Classroom endpoint

--- frontend/components/Classroom/StaffClassroom/CreateClassroom.tsx
+++ frontend/components/Classroom/StaffClassroom/CreateClassroom.tsx
@@ -1,15 +1,74 @@
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
+    DialogClose,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
+  import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+  } from "@/components/ui/form"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useAppSelector } from '@/redux/store';
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from 'next/navigation'
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { BsFileEarmarkPlus } from "react-icons/bs"
 
+
+const formSchema = z.object({
+  classroom: z.string().min(4, {
+    message: "Username must be at least 4 characters.",
+  }),
+  
+})
+
 const CreateClassroom = () => {
+  const { toast } = useToast()
+  const router = useRouter()
+  const uid = useAppSelector((state) => state.uidReducer.value.userID)
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      classroom: ""
+    },
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const res = await fetch("http://localhost:8000/api/classroom/create/",{
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      credentials: 'include',
+      body: JSON.stringify({
+          "staff_profile_id": uid,
+          "name": values.classroom
+      })
+    })
+    if (res.ok){
+      toast({
+        title: `Classroom ${values.classroom} created`,
+        description: `Successfully created ${values.classroom}`,
+      })
+      await form.reset();
+      await router.push('/classroom')
+    }
+    else {
+      toast({
+        variant: "destructive",
+        title: `${res.status} Failed to create new Classroom`,
+      })
+      await router.refresh()
+    }
+  }
+
   return (
     <div>
         <Dialog>
@@ -22,10 +81,26 @@ const CreateClassroom = () => {
         <DialogContent>
             <DialogHeader>
             <DialogTitle>Create A New Classroom</DialogTitle>
-            <DialogDescription>
-                Lets create a classroom
-            </DialogDescription>
             </DialogHeader>
+            <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="classroom"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input autoComplete='off' placeholder="Classroom Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DialogClose asChild>
+                <Button className='w-full' type="submit" >Create</Button>
+              </DialogClose>
+            </form>
+          </Form>
         </DialogContent>
         </Dialog>
     </div>
--- frontend/components/Classroom/StaffClassroom/StaffClassroomList.tsx
+++ frontend/components/Classroom/StaffClassroom/StaffClassroomList.tsx
@@ -7,6 +7,8 @@ import {
 } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 import CreateClassroom from '@/components/Classroom/StaffClassroom/CreateClassroom'
+import DeleteClassroom from './DeleteClassroom'
+
 
 interface Teacher {
     teacher_profile_id: string;
@@ -53,10 +55,13 @@ const StaffClassroomList = () => {
     <div>
         <div className='grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 pt-6 pb-8'>
             {classrooms.map((classroom) => (
-                <Card className='h-32'>
+                <Card className='h-32 flex justify-between items-center'>
                 <CardHeader>
                 <CardTitle>{classroom.name}</CardTitle>
                 </CardHeader>
+                <div className='px-6'>
+                  <DeleteClassroom classroomName={classroom.name} classroomID={classroom.class_id}/>
+                </div>
             </Card>
             ))}
         </div>
