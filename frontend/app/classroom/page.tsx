58767bb34214588aab0971db5885e974d93d388a
AlifHossain27 <alifh044@gmail.com>
2024-01-17 15:27:00 +0600

    Adding Loading in Classroom List

--- frontend/app/classroom/page.tsx
+++ frontend/app/classroom/page.tsx
@@ -1,9 +1,10 @@
-"use client"
+'use client'
 import { useAppSelector } from '@/redux/store';
 import ClassroomList from '@/components/Classroom/ClassroomList';
 import StaffClassroomList from '@/components/Classroom/StaffClassroom/StaffClassroomList'
 import { Separator } from "@/components/ui/separator"
-
+import { Suspense } from 'react';
+import { ImSpinner2 } from "react-icons/im";
 
 
 
@@ -16,9 +17,15 @@ const page = () => {
             <h1>Classrooms</h1>
         </div>
         <Separator />
+        <Suspense fallback={
+          <div className='pt-20'>
+            <ImSpinner2 className= "animate-spin" size= "50"/>
+          </div>
+        }>
         <div className='pt-6'>
                 {user_type === 'staff' ? <StaffClassroomList /> : <ClassroomList />}
         </div>
+        </Suspense>
     </div>
   )
 }
