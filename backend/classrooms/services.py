f82fccad4db89272befd2542898ae20c48e6eaa5
AlifHossain27 <alifh044@gmail.com>
2024-02-06 12:26:00 +0600

    Adding Classroom views backend

---
+++ backend/classrooms/services.py
@@ -0,0 +1,106 @@
+import dataclasses
+from typing import TYPE_CHECKING
+from .models import Classroom
+from profiles.models import StaffProfile, TeacherProfile, StudentProfile
+from rest_framework import response, exceptions, status
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+
+
+@dataclasses.dataclass
+class ClassroomCreateDataClass:
+    name: str
+    staff: str 
+    class_id:str = None
+    id: int = None
+
+    @classmethod
+    def from_instance(cls, classroom_model: "Classroom"):
+        return cls(
+            class_id = classroom_model.class_id,
+            name = classroom_model.name,
+            staff = classroom_model.staff,
+            id = classroom_model.id
+        )
+
+@dataclasses.dataclass
+class ClassroomDataClass:
+    name: str
+    staff: str 
+    teachers: str = None
+    students: str = None
+    class_id:str = None
+    id: int = None
+
+    @classmethod
+    def from_instance(cls, classroom_model: "Classroom"):
+        return cls(
+            class_id = classroom_model.class_id,
+            name = classroom_model.name,
+            staff = classroom_model.staff,
+            teachers = classroom_model.teachers,
+            students = classroom_model.students,
+            id = classroom_model.id
+        )
+
+# Create Classroom
+def create_classroom(classroom_dc: "ClassroomCreateDataClass") -> "ClassroomCreateDataClass":
+    instance = Classroom(
+        staff = get_object_or_404(StaffProfile, profile_uid = classroom_dc.staff),
+        name = classroom_dc.name, 
+    )
+    instance.save()
+    return ClassroomCreateDataClass.from_instance(instance)
+    
+# Retrieve Individual Classroom
+def get_classroom(class_id: str) -> "ClassroomDataClass":
+    classroom = get_object_or_404(Classroom, class_id=class_id)
+    return ClassroomDataClass.from_instance(classroom_model= classroom)
+
+# Retrieve Classroom List
+def get_classrooms(user_type: str, profile_uid: str) -> list['ClassroomDataClass']:
+    if user_type == 'staff':
+        try:
+            staff = StaffProfile.objects.filter(profile_uid=profile_uid).first()
+        except StaffProfile.DoesNotExist:
+            return response.Response({"detail": "Staff not found"}, status=status.HTTP_404_NOT_FOUND)
+        classrooms = Classroom.objects.filter(staff=staff)
+        return [ClassroomDataClass.from_instance(classroom) for classroom in classrooms]
+    if user_type == 'teacher':
+        teacher = get_object_or_404(TeacherProfile, profile_uid=profile_uid)
+        classrooms = Classroom.objects.filter(teachers=teacher)
+        return [ClassroomDataClass.from_instance(classroom) for classroom in classrooms]
+    if user_type == 'student':
+        student = get_object_or_404(StudentProfile, profile_uid=profile_uid)
+        classrooms = Classroom.objects.filter(students=student)
+        return [ClassroomDataClass.from_instance(classroom) for classroom in classrooms]
+    return []
+
+# Add Teacher to Classroom
+def add_teacher_to_classroom(class_id: str, profile_uid: str) -> "ClassroomDataClass":
+    classroom = get_object_or_404(Classroom, class_id=class_id)
+    teacher= get_object_or_404(TeacherProfile, profile_uid=profile_uid)
+    classroom.teachers.add(teacher)
+    return ClassroomDataClass.from_instance(classroom_model= classroom)
+
+# Remove Teacher from Classroom
+def remove_teacher_from_classroom(class_id: str, profile_uid: str) -> "ClassroomDataClass":
+    classroom = get_object_or_404(Classroom, class_id=class_id)
+    teacher= get_object_or_404(TeacherProfile, profile_uid=profile_uid)
+    classroom.teachers.remove(teacher)
+    return ClassroomDataClass.from_instance(classroom_model= classroom)
+
+# Add Student to Classroom
+def add_student_to_classroom(class_id: str, profile_uid: str) -> "ClassroomDataClass":
+    classroom = get_object_or_404(Classroom, class_id=class_id)
+    student= get_object_or_404(StudentProfile, profile_uid=profile_uid)
+    classroom.students.add(student)
+    return ClassroomDataClass.from_instance(classroom_model= classroom)
+
+# Remove Student from Classroom
+def remove_student_to_classroom(class_id: str, profile_uid: str) -> "ClassroomDataClass":
+    classroom = get_object_or_404(Classroom, class_id=class_id)
+    student= get_object_or_404(StudentProfile, profile_uid=profile_uid)
+    classroom.students.remove(student)
+    return ClassroomDataClass.from_instance(classroom_model= classroom)
---
+++ backend/classrooms/views.py
@@ -0,0 +1,82 @@
+from rest_framework import views, response, status
+from .serializers import ClassroomSerializer, ClassroomCreateSerializer, AddRemoveTeacherSerializer
+from . import services
+from users.permissions import IsAdministrator, IsStaff, IsTeacher, IsStudent
+from users.authentication import Authentication
+
+# Create Classroom View
+class CreateClassroom(views.APIView):
+    authentication_classes = [Authentication]
+    permission_classes = [IsStaff]
+    def post(self, request):
+        serializer = ClassroomCreateSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        serializer.instance = services.create_classroom(classroom_dc = data)
+        return response.Response(data=serializer.data)
+
+# Retrieve Individual Classroom View
+class RetrieveClassroom(views.APIView):
+    authentication_classes = [Authentication]
+    permission_classes = [IsAdministrator | IsStaff | IsTeacher | IsStudent]
+    def get(self, request, class_id):
+        classroom = services.get_classroom(class_id)
+        serializer = ClassroomSerializer(classroom)
+        return response.Response(data=serializer.data, status=status.HTTP_200_OK)
+
+# Retrieve Classroom List View
+class ListClassrooms(views.APIView):
+    authentication_classes = [Authentication]
+    permission_classes = [IsAdministrator | IsStaff | IsTeacher | IsStudent]
+    def get(self, request, user_type, profile_uid):
+        classrooms= services.get_classrooms(user_type=user_type, profile_uid=profile_uid)
+        serializer = ClassroomSerializer(classrooms, many = True)
+        return response.Response(data=serializer.data, status=status.HTTP_200_OK)
+
+# Add Teacher to Classroom View 
+class AddTeacher(views.APIView):
+    authentication_classes = [Authentication]
+    permission_classes = [IsStaff]
+    def post(self, request):
+        serializer = AddRemoveTeacherSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        class_id = serializer.validated_data['class_id']
+        teacher = serializer.validated_data['teachers']
+        serializer.instance = services.add_teacher_to_classroom(class_id=class_id, profile_uid= teacher)
+        return response.Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+# Remove Teacher from Classroom View
+class RemoveTeacher(views.APIView):
+    authentication_classes = [Authentication]
+    permission_classes = [IsStaff]
+    def post(self, request):
+        serializer = AddRemoveTeacherSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        class_id = serializer.validated_data['class_id']
+        teacher = serializer.validated_data['teachers']
+        serializer.instance = services.remove_teacher_from_classroom(class_id=class_id, profile_uid= teacher)
+        return response.Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+# Add Student to Classroom View 
+class AddStudent(views.APIView):
+    authentication_classes = [Authentication]
+    permission_classes = [IsStaff]
+    def post(self, request):
+        serializer = AddRemoveTeacherSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        class_id = serializer.validated_data['class_id']
+        student = serializer.validated_data['students']
+        serializer.instance = services.add_student_to_classroom(class_id=class_id, profile_uid= student)
+        return response.Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+# Remove Student from Classroom View
+class RemoveStudent(views.APIView):
+    authentication_classes = [Authentication]
+    permission_classes = [IsStaff]
+    def post(self, request):
+        serializer = AddRemoveTeacherSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        class_id = serializer.validated_data['class_id']
+        student = serializer.validated_data['students']
+        serializer.instance = services.remove_student_from_classroom(class_id=class_id, profile_uid= student)
+        return response.Response(data=serializer.data, status=status.HTTP_200_OK)
