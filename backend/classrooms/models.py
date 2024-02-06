746591f9af6216e682736b72b0658693e4aa592c
AlifHossain27 <alifh044@gmail.com>
2024-02-06 12:25:12 +0600

    Adding Classroom model backend

---
+++ backend/classrooms/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from profiles.models import StaffProfile, TeacherProfile, StudentProfile
+from django.utils import timezone
+
+class Classroom(models.Model):
+    class_id = models.CharField(max_length=20,unique=True, verbose_name="Class ID")
+    name = models.CharField(max_length=100, verbose_name="Classroom Name")
+    staff = models.ForeignKey("profiles.StaffProfile", on_delete=models.CASCADE, related_name='classrooms')
+    teachers = models.ManyToManyField("profiles.TeacherProfile", related_name='classrooms', blank=True)
+    students = models.ManyToManyField("profiles.StudentProfile", related_name='classrooms', blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.class_id:
+            current_time = timezone.now()
+            unique_number = self.__class__.objects.count() + 1
+            self.class_id = f"CLASSROOM-{current_time.strftime('%Y')}-{unique_number:04d}"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("profiles.urls")),
+    path("api/", include("classrooms.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
