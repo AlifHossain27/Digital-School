541e6dafa3ff3bcd511b53627e1788156acf20e0
AlifHossain27 <alifh044@gmail.com>
2024-03-18 13:13:12 +0600

    Classwork list backend connection

--- frontend/components/Classwork/ClassworkList.tsx
+++ frontend/components/Classwork/ClassworkList.tsx
@@ -10,7 +10,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '@/redux/store'
 import { useAppSelector } from '@/redux/store';
-import getClassworks from '@/actions/getClassworks'
+import { getClassworks } from "@/actions/classwork";
 import { Button } from "../ui/button";
 import EditClasswork from "./EditClasswork";
 import DeleteClasswork from "./DeleteClasswork";
@@ -19,11 +19,13 @@ import { SetClasswork } from "@/redux/features/classwork-slice";
 import { ImSpinner2 } from "react-icons/im";
 
 interface Classwork {
-    id: number,
-    teacher_profile_id: string,
-    title: string,
-    description: string,
-    due_date: any
+    classwork_id: string;
+    title: string;
+    description: string;
+    due_date: string;
+    teacher: string;
+    classroom: string;
+    students: string;
 }
 
 const ClassworkList = () => {
@@ -44,25 +46,25 @@ const ClassworkList = () => {
             </div>)}
         {classworks?.map((classwork: Classwork,i: number) => {
             return (
-            <Accordion type="single" collapsible className="text-xl" key={classwork.id}>
+            <Accordion type="single" collapsible className="text-xl" key={classwork.classwork_id}>
                 <AccordionItem value={`item-${i}`}>
                 <AccordionTrigger>{classwork.title}</AccordionTrigger>
                 <AccordionContent className="text-lg">
                     {classwork.description}
                     {userType==="teacher" ? (
                     <div className="pt-6 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-                        <Link href='/classroom/classwork/submissions/' onClick={() => dispatcher(SetClasswork(classwork.id))}>
+                        <Link href='/classroom/classwork/submissions/' onClick={() => dispatcher(SetClasswork(classwork.classwork_id))}>
                             <Button className="rounded-full w-full">View Submissions</Button>
                         </Link>
                         <EditClasswork 
-                        classworkID={classwork.id} 
+                        classworkID={classwork.classwork_id} 
                         classworkTitle={classwork.title} 
                         classworkDescription={classwork.description}
                         classworkDueDate={classwork.due_date}/>
-                        <DeleteClasswork classworkID={classwork.id} classworkTitle={classwork.title}/>
+                        <DeleteClasswork classworkID={classwork.classwork_id} classworkTitle={classwork.title}/>
                     </div>
                     ):(<div className="pt-6 flex flex-row gap-6">
-                        <Link href='/classroom/classwork/view/' onClick={() => dispatcher(SetClasswork(classwork.id))}>
+                        <Link href='/classroom/classwork/view/' onClick={() => dispatcher(SetClasswork(classwork.classwork_id))}>
                             <Button className="rounded-full">View Classwork</Button>
                         </Link>
                     </div>)}
