e220229aefc966d52694f643a347ec14c9abfa48
AlifHossain27 <alifh044@gmail.com>
2024-03-26 10:54:31 +0600

    Connecting to Class Comment backend

--- frontend/actions/classwork.ts
+++ frontend/actions/classwork.ts
@@ -45,3 +45,15 @@ export async function getClassworkSubmissionDetails(classworkID: string, classro
     const data = await resp.json();
     return data
 }
+
+export async function getPublicComments(classworkID: string) {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const resp = await fetch(`http://localhost:8000/api/classwork/${classworkID}/public-comment/`, {
+        credentials: 'include',
+    });
+    if (!resp.ok) {
+        window.location.replace('/classroom');
+    }
+    const data = await resp.json();
+    return data
+}
