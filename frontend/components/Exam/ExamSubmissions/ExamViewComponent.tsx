754e3f36da14f5d2a0f755e4e1b4b507209dfef1
AlifHossain27 <alifh044@gmail.com>
2024-05-15 07:33:36 +0600

    Updating Exam View Component

--- frontend/components/Exam/ExamSubmissions/ExamViewComponent.tsx
+++ frontend/components/Exam/ExamSubmissions/ExamViewComponent.tsx
@@ -4,6 +4,8 @@ import { useQuery } from '@tanstack/react-query';
 import { getExamSubmission } from '@/actions/exam';
 import { ImSpinner2 } from "react-icons/im";
 import { FormElementInstance, FormElements } from '../ExamForm/FormElements'
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 interface Student {
     profile_uid: string,
@@ -49,20 +51,64 @@ const ExamViewComponent = ({examID,content,submissionID}: {content: FormElementI
     const submissionData = `${submittedData?.content ?? '[]'}`
     const submissionContent = JSON.parse(submissionData) as FormElementInstance[]
 
+    let assignedMarks: String[] = []
+    let hasHelperText = false
+    const [mark, setMark] = useState<string>('');
+    const [marks, setMarks] = useState<string[]>([]);
     return (
-        <div className='flex justify-center w-full h-full items-center p-8'>
-            <div className='max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto shadow-xl shadow-blue-700 rounded opacity-80 cursor-not-allowed pointer-events-none'>
+        <div className='flex flex-col gap-8 justify-center w-full h-full items-center p-8'>
+            <div className='max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto shadow-lg shadow-blue-700 rounded opacity-80'>
                 {
-                    content.map((element) => {
+                    content.map((element, i) => {
                         const FormElement = FormElements[element.type].formComponent;
-                        return <FormElement 
-                                key={element.id} 
-                                elementInstance={element} 
-                                defaultValue= {String(submissionContent[Number(element.id)]) || ''}
-                                />
+                        if (element?.extraAttributes?.helperText) {
+                            hasHelperText = true
+                            assignedMarks.push(element.extraAttributes.helperText)
+                        }
+                        return (
+                        <div key={i}>
+                            <div className='cursor-not-allowed pointer-events-none'>
+                                <FormElement 
+                                    key={element.id} 
+                                    elementInstance={element} 
+                                    defaultValue= {String(submissionContent[Number(element.id)]) || ''}
+                                    />
+                            </div>
+                            {element?.extraAttributes?.helperText ? (
+                                <div className='flex gap-4 pt-2'>
+                                    <Input type='number' 
+                                        className='w-40 h-8 border-t-[0.4px] border-l-[0.4px] border-r-[0.4px]' 
+                                        max={element.extraAttributes.helperText} 
+                                        onChange={async(e)=>{ 
+                                            e.preventDefault()
+                                            const newValue = e.target.value;
+                                            if(newValue > e.target.max) { 
+                                                e.target.value = '0'
+                                            } else {
+                                                const newMark = newValue !== '' ? newValue : '0'
+                                                setMark(newMark);
+                                                setMarks(prevMarks => {
+                                                    
+                                                    const updatedMarks = prevMarks.filter(mark => mark !== newMark)
+                                                    
+                                                    return [newMark, ...updatedMarks]
+                                                })
+                                            }}}/>
+                                    <h1 className='pt-1'>{assignedMarks[i]}</h1>
+                                </div>
+                            ) : (
+                                <div></div>
+                            )}
+                        </div>)
                     })   
                 }
             </div>
+            {hasHelperText && (
+                <div className='flex justify-between max-w-[620px] bg-background w-full p-8 shadow-xl shadow-green-700 rounded opacity-80'>
+                    <h1 className='pt-2'>Total Marks Obtained: {marks.map(Number).reduce((acc, curr) => acc + curr, 0)} / {assignedMarks.map(Number).reduce((acc, curr) => acc + curr, 0)}</h1>
+                    <Button className='bg-green-600 h-10 rounded-none'>Submit Marks</Button>
+                </div>
+            )}
         </div>
     )
 }
